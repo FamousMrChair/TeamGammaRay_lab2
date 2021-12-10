893cf0ff3f90a33c9cb56e89e2f24889823b2abb
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-12-10 12:16:33 -0500

    Update ALTester.java

--- lab02/repo/ALTester.java
+++ lab02/repo/ALTester.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 public class ALTester {
-  
   public static void main(String[] args){
     ArrayList potato = new ArrayList();
+    System.out.println(potato);
+    for (int i = 0; i < 23; i ++){
+      potato.add((int)(Math.random()*23));
+    }
     System.out.println(potato);
   }
 }
