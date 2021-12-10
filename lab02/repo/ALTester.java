8c6a956e5af92713be76aa2e095a18c9f0283f90
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-12-10 12:23:03 -0500

    Update ALTester.java

--- lab02/repo/ALTester.java
+++ lab02/repo/ALTester.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
 public class ALTester {
+  public boolean sort(ArrayList<Integer> foo){
+    for (int i = 0; i < foo.length; i++){
+      if (foo[i + 1] < foo[i]){
+        return false;
+      }
+    }
+    return true;
+  }
   public static void main(String[] args){
     ArrayList<Integer> potato = new ArrayList<Integer>();
     System.out.println(potato);
@@ -7,5 +15,6 @@ public class ALTester {
       potato.add((int)(Math.random()*23));
     }
     System.out.println(potato);
+    System.out.println(sort(potato));
   }
 }
