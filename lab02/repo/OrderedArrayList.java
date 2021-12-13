76a7c2894b9c279014cb5bf5398ddf3ee6196cf4
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-12-12 22:09:34 -0500

    Update OrderedArrayList.java

--- lab02/repo/OrderedArrayList.java
+++ lab02/repo/OrderedArrayList.java
@@ -1,1 +1,19 @@
+import java.util.ArrayList;
 
+public class OrderedArrayList{
+  public ArrayList<Integer> oal;
+  public OrderedArrayList(){
+    oal = new ArrayList<Integer>();
+  }
+  
+  public int size(){
+    return oal.size();
+  }
+  public String toString(){
+    return oal.toString();
+  }
+  public int get(int index){
+    return oal(index);
+  }
+}
+  
