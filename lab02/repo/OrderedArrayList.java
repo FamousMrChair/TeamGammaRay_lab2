035e03dd4bd54d8730b4a0f998c4f88c2a3f189d
Jun Hong Wang <jwang31@stuy.edu>
2021-12-13 12:34:23 -0500

    oal

--- lab02/repo/OrderedArrayList.java
+++ lab02/repo/OrderedArrayList.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class OrderedArrayList{
-  public ArrayList<Integer> oal;
+  private ArrayList<Integer> oal;
   public OrderedArrayList(){
     oal = new ArrayList<Integer>();
   }
@@ -12,15 +12,16 @@ public class OrderedArrayList{
   public String toString(){
     return oal.toString();
   }
-  public int get(int index){
+  public Integer get(int index){
     return oal.get(index);
   }
-  public int remove(int index){
+  public Integer remove(int index){
     return oal.remove(index);
   }
-  public boolean add(int val){
+  public boolean addLinear(Integer val){
     int i = 0;
-    while (i < oal.size()){
+    int s = oal.size();
+    while (i < s){
       if (val < oal.get(i)){
         oal.add(i,val);
         return true;
@@ -31,17 +32,39 @@ public class OrderedArrayList{
     return true;
   }
 
+  public void addBinary(Integer val) {
+    int min = 0;
+    int max = size();
+    int index = (size() - 1) / 2;
+    while (min != max) {
+      if (val > oal.get(index)) {
+        min = index;
+        max = size() - 1;
+        index = (int) Math.floor((max - min) / 2);
+      } else {
+        max = index;
+        min = 0;
+        index = (int) Math.floor((max - min) / 2);
+      }
+      System.out.println(min + "," + max + "," + index);
+    }
+    oal.add(index, val);
+  }
+
   public static void main(String[] args) {
     OrderedArrayList testing = new OrderedArrayList();
     System.out.println(testing.size());
     for (int i = 0; i < 23; i++) {
-      testing.add(i);
+      testing.addLinear(i);
     }
     //adding elements out of order, arraylist maintains order
-    System.out.println(testing.add(1));
-    System.out.println(testing.add(15));
-    System.out.println(testing.add(3));
-    System.out.println(testing.add(1234));
+    testing.addLinear((Integer)1);
+    testing.addLinear(15);
+    testing.addLinear(3);
+    testing.addLinear(1234);
+    System.out.println(testing.size());
+    System.out.println(testing.toString());
+    testing.addBinary(25);
     System.out.println(testing.toString());
   }
 }
